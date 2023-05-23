290b8e0a0c3b9adb94d2cde851cf035dff486a44
adamgrharvey <adamgrharvey@gmail.com>
2023-05-22 23:32:41 -0700

    spotify user auth

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,17 @@ function App() {
   const [expiresIn, setExpiresIn] = useState(0);
   const [tokenRefresh, setTokenRefresh] = useState(true);
 
+  const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID; // insert your client id here from spotify
+  const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const REDIRECT_URL_AFTER_LOGIN = "http://localhost:3000/";
+  const SPACE_DELIMITER = "%20";
+  const SCOPES = [
+    "user-read-currently-playing",
+    "user-read-playback-state",
+    "playlist-read-private",
+  ];
+  const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER);
+
   useEffect(() => {
     if (tokenRefresh) {
       if (expiresIn === 0) {
@@ -23,8 +34,14 @@ function App() {
     }
   })
 
+  const handleLogin = () => {
+    window.location.href = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
+  };
+
   return (
     <div className="App">
+      <h1>hi</h1>
+      <button onClick={handleLogin}>login to spotify</button>
     </div>
   );
 }
