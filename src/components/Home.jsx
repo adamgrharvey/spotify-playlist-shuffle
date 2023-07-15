d94ec9a479dd8a79476f259d4c8e5007f098e6a8
adamgrharvey <adamgrharvey@gmail.com>
2023-07-15 00:00:05 -0700

    Separates PlaylistHead into its own component for my sanity

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,6 +11,8 @@ import shufflePlaylistInPlace from "./helpers/shufflePlaylistInPlace";
 import removeNonUserPlaylists from "./helpers/removeNonUserPlaylists";
 import countPlaylistDuration from "./helpers/countPlaylistDuration";
 import Track from "./Track";
+import PlaylistHead from "./PlaylistHead";
+
 
 export default function Home(props) {
 
@@ -165,24 +167,7 @@ export default function Home(props) {
             <button onClick={() => shuffleTracks(currPlaylist.shuffleTracks, setCurrPlaylist)}>shuffle</button>
             <button onClick={() => shufflePlaylistInPlace(userAuth, currPlaylist, setCurrPlaylist)}>save</button>
 
-            <div className="PlaylistTitleImage">
-              <div><img width={200} height={200} src={currPlaylist.data.images[0].url} alt="PlaylistImage"></img></div>
-              <div>
-                <div className={`PlaylistTitleLarge-${htmlDecode(currPlaylist.data.name).length > 16 ? `over11char` : `under12char`}`}>{htmlDecode(currPlaylist.data.name)}</div>
-                <div className="PlaylistDesc">{htmlDecode(currPlaylist.data.description)} </div>
-                <div className="PlaylistDetails">
-                  <span className="PlaylistOwner">{`${htmlDecode(currPlaylist.data.owner.display_name)} `}</span>
-                  <span>
-                    {currPlaylist.data.followers.total > 0 ? <span className="DotSpace">{`• ${currPlaylist.data.followers.total} likes `}</span> : ``}
-                  </span>
-                  <span className="DotSpace">{`• ${currPlaylist.tracks.length} songs, `}
-                  </span>
-                  <span className="PlaylistDuration">{countPlaylistDuration(currPlaylist)}
-                  </span>
-                </div>
-              </div>
-
-            </div>
+            <PlaylistHead currPlaylist={currPlaylist} />
 
 
             <div className="contentSpacing">
@@ -229,9 +214,4 @@ export default function Home(props) {
       </div>
     </>
   )
-}
-
-function htmlDecode(input) {
-  var doc = new DOMParser().parseFromString(input, "text/html");
-  return doc.documentElement.textContent;
 }
